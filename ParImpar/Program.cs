17c6e1ab802e03c0282b550f4555de4eb7c5780a
Washinfron Fernandes <wmfmg@hotmail.com>
2023-01-24 16:43:08 -0300

    Projeto Finalizado

---
+++ ParImpar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero;
+            
+            Console.WriteLine("Digite o número: ");
+
+            numero = float.Parse(Console.ReadLine());
+
+            if (numero % 2 ==  0) 
+            {
+                Console.WriteLine("O número informado é PAR. ");
+
+            }
+            else
+            {
+                Console.WriteLine("O número informado é ÍMPAR. ");
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
